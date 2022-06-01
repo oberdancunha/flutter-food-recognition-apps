879ed2ddb16479d8a7133a692664cd618d8a44fb
oberdancunha <oberdancunha@gmail.com>
2022-06-01 12:05:06 -0300

    Install the image_picker package in the dependency module application and make it available in the core and main applications.

--- flutter_food_recognition_dependency_module/lib/flutter_food_recognition_dependency_module.dart
+++ flutter_food_recognition_dependency_module/lib/flutter_food_recognition_dependency_module.dart
@@ -2,6 +2,7 @@ library flutter_food_recognition_modules;
 
 export 'package:equatable/equatable.dart';
 export 'package:fpdart/fpdart.dart';
+export 'package:image_picker/image_picker.dart';
 export 'package:kt_dart/kt.dart';
 export 'package:provider/provider.dart';
 export 'package:tflite/tflite.dart';
