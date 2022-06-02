33fbc14ca06ec212b9bfdae37a3d7c38329ad75e
oberdancunha <oberdancunha@gmail.com>
2022-06-02 17:56:49 -0300

    SetPreferredOrientations to only portraitUp.

--- flutter_food_recognition_app/lib/main.dart
+++ flutter_food_recognition_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'core/app_widget.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const AppWidget());
 }
